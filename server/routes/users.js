82bf925634e4f4503d70ac804f8f3bd348ed75d0
Clint <clinthiles1992@gmail.com>
2025-02-25 11:23:59 -0800

    Add Sequelize and PostgreSQL support; implement user model and delete route

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,8 @@ const { v4: uuidv4 } = require("uuid");
 
 // In-memory storage for users
 let users = [];
+// Counter for user IDs - starts at 1
+let nextUserId = 1;
 
 // GET all users
 router.get("/", function (req, res, next) {
@@ -16,7 +18,7 @@ router.get("/", function (req, res, next) {
 // Create user
 router.post("/", (req, res, next) => {
   const user = {
-    id: uuidv4(),
+    id: nextUserId++,
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
@@ -36,6 +38,26 @@ router.put("/:userId", (req, res, next) => {
   res.status(200).json({
     message: "Updated user!",
   });
+});
+
+router.delete("/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+
+  const userIndex = users.findIndex((user) => user.id.toString() === userId);
+
+  if (userIndex !== -1) {
+    // Remove the user from the array
+    const deletedUser = users.splice(userIndex, 1)[0];
+
+    res.status(200).json({
+      message: "User deleted successfully",
+      deletedUser: deletedUser,
+    });
+  } else {
+    res.status(404).json({
+      message: "User not found",
+    });
+  }
 });
 
 module.exports = router;
