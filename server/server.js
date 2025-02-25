f08711c20347a25a2a787c40096bf6652c30baf4
Clint <clinthiles1992@gmail.com>
2025-02-25 10:24:36 -0800

    Server set up for project 70% done

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Add middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.😎😎😎😎`);
+});
