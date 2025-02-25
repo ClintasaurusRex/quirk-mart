355f3316f4eb6c7345a623f03235874f4e82b326
Clint <clinthiles1992@gmail.com>
2025-02-25 11:04:22 -0800

    Added a uuid package for unique id

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,27 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
 
-// GET
+// In-memory storage for users
+let users = [];
+
+// GET all users
 router.get("/", function (req, res, next) {
   res.status(200).json({
-    message: "Handling GET requests to /api/users",
+    message: "Successfully retrieved all users",
+    users: users,
   });
 });
 
-// Create
+// Create user
 router.post("/", (req, res, next) => {
   const user = {
+    id: uuidv4(),
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   };
+
+  // Store the new user
+  users.push(user);
+
   res.status(201).json({
-    message: "Handling POST requests to /api/users",
+    message: "User created successfully",
     createdUser: user,
   });
 });
 
-// Delete
+// PUT/ Update
 router.put("/:userId", (req, res, next) => {
   res.status(200).json({
     message: "Updated user!",
