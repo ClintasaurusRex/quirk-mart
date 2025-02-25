febda888381e0599aad9a8151327e71d95c07667
Clint <clinthiles1992@gmail.com>
2025-02-25 15:03:17 -0800

    Remove commented-out user creation and login routes for cleaner code

--- server/routes/users.js
+++ server/routes/users.js
@@ -10,12 +10,6 @@ router.get("/", userController.getAllUsers);
 // GET single user by ID
 router.get("/:userId", userController.getUserById);
 
-// // Create user
-// router.post("/register", userController.createUser);
-
-// // Login user
-// router.post("/login", userController.login);
-
 // Update user
 router.put("/:userId", userController.updateUser);
 
