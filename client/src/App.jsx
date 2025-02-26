f91a852a646c1177f287010b576796b7ee7f476d
Nelson Santos <nelsoncgsantos@gmail.com>
2025-02-25 19:23:53 -0600

    added dark mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import './styles/main.scss';
+
+
 import Home from './pages/Home';
 import Products from './pages/Products';
 import NotFound from './pages/NotFound';
@@ -7,17 +11,23 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="app-container">
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <ThemeProvider>
+      <div className='App'>
+        <div className="app-container">
+          <Navbar />
+            
+       
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
